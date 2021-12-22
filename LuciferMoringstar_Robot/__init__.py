89ba45ec2e7b8eda10f3a2285fe2d26b0d15f6d5
Mohd haneen <90704922+Mohdhaneen@users.noreply.github.com>
2021-12-22 12:59:56 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -28,14 +28,13 @@ Bot Owner Only
 """
 
 ABOUT = """
-➥ Name :  Auto Filter V3
+♻️ Name :  KATY PERRY 
     
-➥ Creator : [Naughty PROFESSOR](
-https://youtube.com/c/NaughtyPROFESSOR)
+♻️ Creator : [MOHDHANEEN](@MOHD_HANEEN)
 
-➥ Language : Python3
+♻️ Language : Python3
 
-➥ Library : Pyrogram Asyncio 1.13.0
+♻️ Library : Pyrogram Asyncio 1.13.0
 
-➥ Tutorial : [Click Me](https://youtu.be/5hnYOKBzyi8)
+♻️ MAINCHANNEL : [Click Me](https://t.me/cinemacenterofficial)
 """
